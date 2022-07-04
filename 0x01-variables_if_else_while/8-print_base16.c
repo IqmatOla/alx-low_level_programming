8d3d8af6ca1249ea1029826aa060ff1da9b5c72b
Your Name <you@example.com>
2022-07-04 16:38:07 -0700

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+/**
+ *
+ *  * main - entry point
+ *
+ *   *
+ *
+ *    * Return: 0
+ *
+ *     */
+
+int main(void)
+
+{
+
+	char a;
+
+	int i;
+
+	for (i = 48; i < 58; i++)
+
+	{
+
+		putchar(i);
+
+	}
+
+	for (a = 'a'; a <= 'f'; a++)
+
+		putchar(a);
+
+	putchar('\n');
+
+	return (0);
+
+}
