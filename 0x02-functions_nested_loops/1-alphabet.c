2a504b69b560dea0e3744daea3a316f773dacad0
Your Name <you@example.com>
2022-07-05 07:47:44 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_alphabet-  print alphabets.
+ * Returns nothing.
+ */
+
+void print_alphabet(void)
+{
+char i = 'a';
+
+for (; i <= 'z'; i++)
+{
+putchar(i);
+}
+putchar('\n');
+}
+
