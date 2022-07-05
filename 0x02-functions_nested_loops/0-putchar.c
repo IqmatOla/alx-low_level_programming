e511084bb6d2be93352ab83eee63db221c79678b
Your Name <you@example.com>
2022-07-05 04:26:44 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+#include <stdio.h>
+
+/**
+ *
+ *  * main - Check description
+ *
+ *   * Description: It prints the word _putchar, followed by a new line.
+ *
+ *    * Return: 0.
+ *
+ *     */
+
+int main(void)
+
+{
+
+		printf("_putchar\n");
+
+
+
+			return (0);
+
+}
