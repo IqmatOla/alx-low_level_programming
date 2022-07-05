9bb47654baf0262245a1e792d179e9df57aa23de
Your Name <you@example.com>
2022-07-05 11:34:18 -0700

    5-sign.c

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,24 @@
+#include "mian.h"
+/**
+ *  print_sign()- prints the sign of a number
+ *  @n: type int
+ *  Return: 1 if positive or 0 if zero or -1 if negative
+ */
+int print_sign(int n)
+{
+if (n > 0)
+{
+_putchar('+');
+return (1);
+}
+else if (n == 0)
+{
+_putchar('0');
+return (0);
+}
+else
+{
+_putchar('-');
+return (-1);
+}
+}
