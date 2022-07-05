372d96927698b994725d8a680cd2ffa937d56ca1
Your Name <you@example.com>
2022-07-05 07:41:07 -0700

    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,23 +1,13 @@
 #include "main.h"
 #include <stdio.h>
 /**
- *
- *  * main - Check description
- *
- *   * Description: It prints the word _putchar, followed by a new line.
- *
- *    * Return: 0.
- *
- *     */
-
+ * main - Check description
+ * Description: It prints the word _putchar, followed by a new line.
+ * Return: 0.
+ */
 int main(void)
-
 {
+	printf("_putchar\n");
 
-		printf("_putchar\n");
-
-
-
-			return (0);
-
-}
+	return (0);
+}	
