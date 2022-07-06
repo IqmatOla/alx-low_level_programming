70dd7fcc2a46449f46d293c219d6704f8912e5f8
Your Name <you@example.com>
2022-07-06 12:36:33 -0700

    6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _abs()- compute absolute value of integer
+ * @a: type int
+ * Return: 0
+ */
+int _abs(int a)
+{
+	if (a < 0)
+}
+return (a * -1);
+}
+else
+{
+	return (a);
+}
+}
